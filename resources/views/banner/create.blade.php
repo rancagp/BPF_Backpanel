ea554cf756e9245b9b3a9d6aecd30cc1fd9d8204
rancagp <ranca632@gmail.com>
2025-09-25 15:22:34 +0700

    uppdate

--- resources/views/banner/create.blade.php
+++ resources/views/banner/create.blade.php
@@ -29,7 +29,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Tambah Banner Baruuu</h3>
+                    <h3 class="card-title">Tambah Banner Baru</h3>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
